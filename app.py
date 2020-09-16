b46bb799ece1d5a65268c0f6da868d44d430a8ad
randersonM <m4yllon@gmail.com>
2020-09-15 22:26:11 -0300

    Initial Commit

---
+++ app.py
@@ -0,0 +1,63 @@
+# coding: utf-8
+from flask import Flask, render_template
+from assets import strings
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/<number>')
+def getExtensiveNumber(number):
+    """
+    De acordo com o parâmetro number realiza 3 divisão para saber se o numero está na casa dos milhar, centena ou decimal
+    :param number:
+    :return:
+    """
+    path = "menos " if int(number) < 0 else ""
+    number = abs(int(number))
+
+    path += numberToExtensive(((number // 1000) % 100), strings.milhar)
+
+    if number > 1000 and number % 1000:
+        path += "e "
+
+    path += strings.centena[((number // 100) % 10)] if number != 100 else "cem"
+
+    if number > 100 and number % 100:
+        path += "e "
+
+    path += numberToExtensive((number % 100), "")
+
+    if number == 0:
+        path = "zero"
+
+    context = {"extenso": path}
+    return context
+
+
+def numberToExtensive(number, place):
+    """
+    :param number: numero a ser convertido para forma extensiva no português
+    :param place: posição decimal do numero
+    :return: string com o nome extensivo do parametro number com a casa
+    """
+    extensive = ""
+    if number > 19:
+        extensive += strings.dezenas[number // 10]
+        extensive += "e " if number % 10 != 0 else ""
+        extensive += strings.unidades[number % 10]
+    else:
+        extensive += strings.unidades[number]
+
+    if number:
+        extensive += place
+
+    return extensive
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
---
+++ assets/strings.py
@@ -0,0 +1,15 @@
+unidades = ["", "um ", "dois ", "três ", "quatro ",
+            "cinco ", "seis ", "sete ", "oito ",
+            "nove ", "dez ", "onze ", "doze ",
+            "treze ", "quatorze ", "quinze ",
+            "dezesseis ", "dezessete ", "dezoito ",
+            "dezenove "]
+
+dezenas = ["", "", "vinte ", "trinta ", "quarenta ",
+           "cinquenta ", "sessenta ", "setenta ", "oitenta ",
+           "noventa "]
+
+centena = ["", "cento ", "duzentos ", "trezentos ", "quatrocentos ", "quinhentos ", "seiscentos ", "setecentos ",
+           "oitocentos ", "novecentos "]
+
+milhar = "mil "
